fb9056b164f750adf8c057871bbee2ae2886cd02
unknown <rami.workstation@gmail.com>
2023-10-01 20:08:38 +0400

    navbar ready

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleMenuOpen = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-export default Navbar
+  return (
+    <>
+      <nav className="desktop-navbar">
+        <div className="navbar-logo">Rami Mohamed</div>
+        <ul className="navbar-links">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <nav className="mobile-navbar">
+        <div className="navbar-logo">Rami Mohamed</div>
+        <div
+          className={`menu-icon ${menuOpen ? "open" : ""}`}
+          onClick={handleMenuOpen}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <div className={`mobile-menu-animator ${menuOpen ? "open" : ""}`}>
+            <div className="mobile-menu">
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#experience">Experience</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
