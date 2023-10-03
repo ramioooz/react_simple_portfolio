2b953782621d8e3ceec00d0bc2d7d445e61bebcc
unknown <rami.workstation@gmail.com>
2023-10-03 15:36:32 +0400

    added arrow-icon

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { backendSkills, frontendSkills } from "../constants";
+import { arrow } from "../assets";
 
 const Experience = () => {
   return (
@@ -46,6 +47,9 @@ const Experience = () => {
           </div>
         </div>
       </div>
+      <a href="#projects">
+        <img src={arrow} alt="arrow icon" className="arrow-icon" />
+      </a>
     </section>
   );
 };
