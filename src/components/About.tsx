e835ceeb35a2483c913b6099638e7dc0ff4fe889
unknown <rami.workstation@gmail.com>
2023-10-03 11:20:05 +0400

    about section ready

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { about_pic, arrow, education, experience } from "../assets";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section>
+      <div className="about-container">
+        <div className="small-text">Get to know more</div>
+        <div className="large-text">About Me</div>
+        <div className="about-photo-and-details-container">
+          <img
+            src={about_pic}
+            alt=""
+            className="image-container"
+            style={{ borderRadius: "3rem" }}
+          />
+          <div className="blocks-and-paragraph-container">
+            <div className="about-blocks-container">
+              <div className="about-block">
+                <img
+                  src={experience}
+                  alt="experience img"
+                  className="small-icon"
+                />
+                <h3>Experience</h3>
+                <p style={{ textAlign: "center", color: "gray" }}>
+                  10+ years
+                  <br />
+                  Full Stack Development
+                </p>
+              </div>
+              <div className="about-block">
+                <img
+                  src={education}
+                  alt="education img"
+                  className="small-icon"
+                />
+                <h3>Education</h3>
+                <p style={{ textAlign: "center", color: "gray" }}>
+                  B.Sc. Bachelors Degree
+                </p>
+              </div>
+            </div>
+            <p className="about-paragraph">
+              This is a very long text paragraph. This is a very long text
+              paragraph. This is a very long text paragraph. This is a very long
+              text paragraph. This is a very long text paragraph. This is a very
+              long text paragraph. This is a very long text paragraph. This is a
+              very long text paragraph. This is a very long text paragraph.
+            </p>
+          </div>
+        </div>
+      </div>
+      <a href="#experience">
+        <img src={arrow} alt="arrow icon" className="arrow-icon" />
+      </a>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,20 +4,16 @@ import { profile_pic } from "../assets";
 import { cvlink, profileLinks } from "../constants";
 
 const Profile = () => {
-  
   return (
-    <section>
       <div className="profile-div">
-        <div className="profile-image-container">
-          <img
-            src={profile_pic}
-            alt="profile image"
-            className="profile-image"
-          />
-        </div>
+        <img
+          src={profile_pic}
+          alt="profile image"
+          className="image-container"
+        />
         <div className="profile-context-container">
           <div className="small-text">Hello. I am</div>
-          <div className="big-text">Rami Mohamed</div>
+          <div className="large-text">Rami Mohamed</div>
           <div className="medium-text">Full Stack Developer</div>
           <div className="profile-txt-btns-container">
             <a
@@ -34,18 +30,22 @@ const Profile = () => {
           </div>
           <div className="profile-links-btns-container">
             {profileLinks.map((link, i) => (
-              <a href={link.url} target="_blank" rel="noopener noreferrer" key={i}>
-                  <img
-                    src={link.img}
-                    alt={link.url}
-                    className="profile-link-btn"
-                  />
-                </a>
+              <a
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={i}
+              >
+                <img
+                  src={link.img}
+                  alt={link.url}
+                  className="profile-link-btn"
+                />
+              </a>
             ))}
           </div>
         </div>
       </div>
-    </section>
   );
 };
 
